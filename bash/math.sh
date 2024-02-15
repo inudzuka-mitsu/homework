3445ed90e6221b585dff6a08f65ee8bbc795b565
root <valeriiababina.qa@gmail.com>
2024-02-15 00:57:08 +0000

    Feb 14th homework

---
+++ bash/math.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter number 1: " number_one
+
+read -p "Enter number 2: " number_two
+
+echo "The sum of your numbers is $((number_one + number_two))"
+
+echo "The difference of your numbers is $((number_one -  number_two))"
+
+echo "The product of your numbers is $((number_one * number_two))"
+
+echo "The quotient of your numbers is $((number_one / number_two))"
