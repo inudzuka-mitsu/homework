f423c2113f69b710d4e403658607e076fa78d2df
root <valeriiababina.qa@gmail.com>
2024-02-16 15:10:42 +0000

    Moved older files into a homework1 folder and completed the new homework

---
+++ bash/homework2/grade_generator.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "Enter your mark: " mark
+echo
+
+if [ $mark -ge 90 ] && [ $mark -le 100 ]
+then
+   grade="A"
+
+elif [ $mark -ge 80 ] && [ $mark -le 89 ]
+then
+   grade="B"
+
+elif [ $mark -ge 70 ] && [ $mark -le 79 ]
+then
+   grade="C"
+
+elif [ $mark -ge 60 ] && [ $mark -le 69 ]
+then
+   grade="D"
+
+elif [ $mark -lt 60 ]
+then
+   grade="F"
+
+else
+   echo "Invalid mark"
+fi
+
+echo "Your grade is $grade"
